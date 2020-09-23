79ab77017bf6a5a354e26df28018051235b0323b
Gergely Pék <gergely.pek@stud.uni-corvinus.hu>
2020-09-23 14:11:17 +0200

    commit

--- UserMaintenance/UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/UserMaintenance/Form1.Designer.cs
@@ -58,10 +58,11 @@
             // 
             this.button1.Location = new System.Drawing.Point(406, 152);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(346, 43);
+            this.button1.Size = new System.Drawing.Size(346, 36);
             this.button1.TabIndex = 2;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // listBox1
             // 
