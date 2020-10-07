df489072c5a3c31907a91347a23b43ce5f318da5
Gergely Pék <gergely.pek@stud.uni-corvinus.hu>
2020-10-07 14:44:37 +0200

    commit

--- gyak5/gyak5/Form1.cs
+++ gyak5/gyak5/Form1.cs
@@ -12,10 +12,11 @@ namespace gyak5
 {
     public partial class Form1 : Form
     {
+
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
 
-        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+        List<Entities.PortfolioItem> Portfolios = new List<Entities.PortfolioItem>();
 
         public Form1()
         {
@@ -24,15 +25,49 @@ namespace gyak5
             dataGridView1.DataSource = Ticks;
 
             CreatePortfolio();
+
+            List<decimal> Nyereségek = new List<decimal>();
+            int intervalum = 30;
+            DateTime kezdőDátum = (from x in Ticks select x.TradingDay).Min();
+            DateTime záróDátum = new DateTime(2016, 12, 30);
+            TimeSpan z = záróDátum - kezdőDátum;
+            for (int i = 0; i < z.Days - intervalum; i++)
+            {
+                decimal ny = GetPortfolioValue(kezdőDátum.AddDays(i + intervalum))
+                           - GetPortfolioValue(kezdőDátum.AddDays(i));
+                Nyereségek.Add(ny);
+                Console.WriteLine(i + " " + ny);
+            }
+
+            var nyereségekRendezve = (from x in Nyereségek
+                                      orderby x
+                                      select x)
+                                        .ToList();
+            MessageBox.Show(nyereségekRendezve[nyereségekRendezve.Count() / 5].ToString());
         }
 
         private void CreatePortfolio()
         {
-            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
-            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
-            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+            Portfolios.Add(new Entities.PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolios.Add(new Entities.PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolios.Add(new Entities.PortfolioItem() { Index = "ELMU", Volume = 10 });
 
-            dataGridView2.DataSource = Portfolio;
+            dataGridView2.DataSource = Portfolios;
+        }
+
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolios)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
         }
     }
 }
