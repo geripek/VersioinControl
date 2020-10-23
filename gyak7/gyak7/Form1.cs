435659f22b82f783f7f214f58d2fe2225eb04e16
Gergely Pék <gergely.pek@stud.uni-corvinus.hu>
2020-10-23 18:56:36 +0200

    commit

--- gyak7/gyak7/Form1.cs
+++ gyak7/gyak7/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using gyak7.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,82 @@ namespace gyak7
 {
     public partial class Form1 : Form
     {
+        List<Person> Population = new List<Person>();
+        List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
+        List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
+
         public Form1()
         {
             InitializeComponent();
+            Population = GetPopulation(@"C:\Temp\nép.csv");
+            BirthProbabilities = GetBirthProbability(@"C:\Temp\születés.csv");
+            DeathProbabilities = GetDeathProbability(@"C:\Temp\halál.csv");
         }
+
+        public List<Person> GetPopulation(string csvpath)
+        {
+            List<Person> population = new List<Person>();
+
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    population.Add(new Person()
+                    {
+                        BirthYear = int.Parse(line[0]),
+                        Gender = (Gender)Enum.Parse(typeof(Gender), line[1]),
+                        NbrOfChildren = int.Parse(line[2])
+                    });
+                }
+            }
+
+            return population;
+        }
+
+        public List<BirthProbability> GetBirthProbability(string csvpath)
+        {
+            List<BirthProbability> bprob = new List<BirthProbability>();
+
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    bprob.Add(new BirthProbability()
+                    {
+                        Age = int.Parse(line[0]),
+                        NbrOfChildren = int.Parse(line[1]),
+                        Probability = double.Parse(line[1]),
+                        
+                    });
+                }
+            }
+
+            return bprob;
+        }
+
+        public List<DeathProbability> GetDeathProbability(string csvpath)
+        {
+            List<DeathProbability> dprob = new List<DeathProbability>();
+
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    dprob.Add(new DeathProbability()
+                    {   
+                        Gender = (Gender)Enum.Parse(typeof(Gender), line[0]),
+                        Age = int.Parse(line[1]),
+                        DeathProb = double.Parse(line[2])
+                    });
+                }
+            }
+
+            return dprob;
+        }
+
+
     }
 }
