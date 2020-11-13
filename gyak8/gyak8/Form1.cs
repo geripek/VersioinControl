501d900d028ec4a619ebe13a88cea58bf09f428b
Gergely Pék <gergely.pek@stud.uni-corvinus.hu>
2020-11-13 17:06:30 +0100

    commit

--- gyak8/gyak8/Form1.cs
+++ gyak8/gyak8/Form1.cs
@@ -16,13 +16,19 @@ namespace gyak8
     {
         private List<Toy> _toys = new List<Toy>();
 
+        private Toy _nextToy;
+
         private IToyFactory _factory;
         public IToyFactory Factory
         {
             get { return _factory; }
-            set { _factory = value; }
+            set
+            {
+                _factory = value;
+                DisplayNext();
+            }
         }
-   
+
         public Form1()
         {
             InitializeComponent();
@@ -57,12 +63,22 @@ namespace gyak8
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            Factory = new CarFactory();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            Factory = new BallFactory();
+        }
 
+        private void DisplayNext()
+        {
+            if (_nextToy != null)
+                Controls.Remove(_nextToy);
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = label1.Top + label1.Height + 20;
+            _nextToy.Left = label1.Left;
+            Controls.Add(_nextToy);
         }
 
         private void button3_Click(object sender, EventArgs e)
