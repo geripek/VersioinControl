5f3598f92dcda5da7255c1001282639ac58c381c
Gergely Pék <gergely.pek@stud.uni-corvinus.hu>
2020-11-19 14:28:32 +0100

    commit

--- gyak9/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ gyak9/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -9,10 +9,21 @@ namespace UnitTestExample.Test
 {
     class AccountControllerTestFixture
     {
-        [Test]
+        [Test,
+         TestCase("abcd1234", false),
+         TestCase("irf@uni-corvinus", false),
+         TestCase("irf.uni-corvinus.hu", false),
+         TestCase("irf@uni-corvinus.hu", true)]
         public void TestValidateEmail(string email, bool expectedResult)
         {
-            
+            // Arrange
+            var accountController = new Controllers.AccountController();
+
+            // Act
+            var actualResult = accountController.ValidateEmail(email);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
         }
     }
 }
