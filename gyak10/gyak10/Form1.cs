bcba367608a020aef63fa6bdae8127a3d9efe470
Gergely Pék <gergely.pek@stud.uni-corvinus.hu>
2020-11-25 14:04:22 +0100

    commit

---
+++ gyak10/gyak10/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WorldsHardestGame;
+
+namespace gyak10
+{
+    public partial class Form1 : Form
+    {
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
+        GameController gc = new GameController();
+        GameArea ga;
+        public Form1()
+        {
+            InitializeComponent();
+            ga = gc.ActivateDisplay();
+            this.Controls.Add(ga);
+
+            gc.GameOver += Gc_GameOver;
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            
+            gc.Start(true);
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            label1.Text = string.Format(
+                "{0}. generáció",
+                generation);
+        }
+    }
+}
